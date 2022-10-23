c1d05516a64c9cb1bce5e34f7360b70c1d149bd4
Luiz Fernando <luvite77@gmail.com>
2022-10-23 10:30:23 -0300

    renderizando player

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
 image.src = "./images/pellet-town.png";
-image.onload = () =>{
+image.onload = () => {
     c.drawImage(image, -750, -550);
 }
 
- 
+const playerImage = new Image();
+playerImage.src = "./images/player-down.png"
+playerImage.onload = () => {
+    c.drawImage(playerImage, 0, 0)
+}
+
+
