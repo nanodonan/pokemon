3323576bbcc5e8b0f4bb5a95c82ea9fc8ed6fc11
Luiz Fernando <luvite77@gmail.com>
2022-10-23 11:40:29 -0300

    renderizando personagem no centro da tela

--- index.js
+++ index.js
@@ -7,16 +7,31 @@ canvas.height = 576;
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-const image = new Image();
-image.src = "./images/pellet-town.png";
-image.onload = () => {
-    c.drawImage(image, -750, -550);
-}
+const mapImage = new Image();
+mapImage.src = "./images/pellet-town.png";
 
 const playerImage = new Image();
 playerImage.src = "./images/player-down.png"
-playerImage.onload = () => {
-    c.drawImage(playerImage, 0, 0)
+
+mapImage.onload = () => {
+    //render map with zoom to see the house
+    c.drawImage(mapImage, -785, -650);
+
+    //render cropped player - crop and paste    
+    const playerWidth = playerImage.width / 4
+    const playerHeight = playerImage.height
+
+    c.drawImage(playerImage,
+        0,
+        0,
+        playerWidth,
+        playerHeight, //end of crop 
+        canvas.width / 2 - playerWidth / 2,
+        canvas.height / 2 - playerHeight / 2,
+        playerWidth,
+        playerHeight, //end of paste
+    )
 }
+
 
 
